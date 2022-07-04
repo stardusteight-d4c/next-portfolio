f889b474355d304c3f25da2d9115bf6250ad4664
Gabriel <bellsena@outlook.com>
2022-07-04 15:23:55 -0300

    target _blank

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -64,12 +64,14 @@ const ProjectCard: FunctionComponent<{
               <a
                 href={github_url}
                 className="flex items-center px-4 py-2 space-x-3 text-lg bg-gray-200 dark:bg-dark-200"
+                target="_blank"
               >
                 <AiFillGithub /> <span>Github</span>
               </a>
               <a
                 href={deployed_url}
                 className="flex items-center px-4 py-2 space-x-3 text-lg bg-gray-200 dark:bg-dark-200"
+                target="_blank"
               >
                 <AiFillProject /> <span>Project</span>
               </a>
