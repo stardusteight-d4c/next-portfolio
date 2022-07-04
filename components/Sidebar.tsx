214a4d1ff3114b60b107f3ff34c4843a197df50e
Gabriel <bellsena@outlook.com>
2022-07-04 16:01:42 -0300

    default theme dark

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,7 +8,7 @@ const Sidebar = () => {
   const { theme, setTheme } = useTheme()
 
   const changeTheme = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light')
+    setTheme(theme === 'dark' ? 'light' : 'dark')
   }
 
   return (
@@ -72,7 +72,6 @@ const Sidebar = () => {
         onClick={changeTheme}
         className="w-8/12 px-5 py-2 my-4 text-white bg-black rounded-full cursor-pointer bg-gradient-to-r from-purple to-blue-500 focus:outline-none hover:scale-105 "
       >
-        {/* //TODO remove bg black */}
         Theme
       </button>
     </>
